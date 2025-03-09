a494b6380ac91c9996a4a4e1d1e5e5d80e8ec5b9
Amelie Tam <atam2@students.cs.ubc.ca>
2025-03-09 01:51:21 -0800

    trying to regain my confidence

--- script.js
+++ script.js
@@ -1,20 +1,24 @@
-console.log("email test");
+// console.log("email test");
 
-function getInputValueToJson() {
-    // Get the input field value
-    let inputValue = document.getElementById("userInput").value;
-    document.getElementById("output").innerText = "Entered Value: " + inputValue;
-    let jsonData = { "input": inputValue };
+// function getInputValueToJson() {
+//     // Get the input field value
+//     let inputValue = document.getElementById("userInput").value;
+//     document.getElementById("output").innerText = "Entered Value: " + inputValue;
+//     let jsonData = { "input": inputValue };
 
-    // Send data to backend via Fetch API
-     fetch('/save', {
-          method: 'POST',
-          headers: { 'Content-Type': 'application/json' },
-          body: JSON.stringify(jsonData)
-     }).then(response => response.json())
-         .then(data => document.getElementById("output").innerText = data.message)
-         .catch(error => console.error("Error:", error));
+//     // Send data to backend via Fetch API
+//      fetch('/save', {
+//           method: 'POST',
+//           headers: { 'Content-Type': 'application/json' },
+//           body: JSON.stringify(jsonData)
+//      }).then(response => response.json())
+//          .then(data => document.getElementById("output").innerText = data.message)
+//          .catch(error => console.error("Error:", error));
 
-    // Log to console
-    console.log(inputValue);
+//     // Log to console
+//     console.log(inputValue);
+// }
+
+function getNews() {
+    return "goats";
 }
