23f56d9654ba8dda3c1a4c9299d710ed1f163d4b
eleanor <eleanort@students.cs.ubc.ca>
2025-03-09 11:30:13 -0700

    scripts

--- script.js
+++ script.js
@@ -3,11 +3,9 @@ let emailList = [];
 function addInputValue() {
     // Get the input field value
     let inputValue = document.getElementById("userInput").value;
-    //document.getElementById("output").innerText = "Entered Value: " + inputValue;
 
     if (inputValue !== "") {
         emailList.push(inputValue); // Add value to array
-        //updateList(); // Update the displayed list
         inputValue.value = " "; // Clear input field
     }
 
@@ -15,12 +13,7 @@ function addInputValue() {
     console.log(emailList);
 }
 
-// function showJSON() {
-//     let jsonString = JSON.stringify(emailList, null, 2); // Convert to JSON format
-//     console.log(jsonString); // Log JSON string
-//     alert(jsonString); // Show JSON in an alert (optional)
-// }
-
+// Saving to JSON file format to parse and send emails
 function saveJSON() {
     let jsonString = JSON.stringify(itemsArray, null, 2); // Convert array to JSON
     let blob = new Blob([jsonString], { type: "application/json" }); // Create a Blob
