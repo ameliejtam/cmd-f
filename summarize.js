8b4475f1a7870c6b8d0ddc2d657fb9caa2fa83c7
Amelie Tam <atam2@students.cs.ubc.ca>
2025-03-08 20:16:18 -0800

    privatized api key

--- summarize.js
+++ summarize.js
@@ -1,33 +1,37 @@
-
-const apiKey = "AIzaSyD224Vm2gc6ke7NLtWbOf25sWUUbhGu5vw";
-
 import fs from 'fs';
-
 import { GoogleGenerativeAI } from "@google/generative-ai";
+
+class key {
+    #apiKey = "AIzaSyD224Vm2gc6ke7NLtWbOf25sWUUbhGu5vw";
+    constructor() {
+        this.apiKey
+    }
+
+    get apiKey() {
+        return this.#apiKey;
+    }
+}
+
+const apiKey = new key().apiKey;
 const genAI = new GoogleGenerativeAI(apiKey);
 
 const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
 
-const prompt = `Summarize three recent climate events using this schema: Return: Array<string>`;
+const prompt = `Summarize three recent climate events, max 50 words each`;
 
-Info = {'climateNews': string}
-Return: Array<Info>`;
+console.log("yellow orange");
 
 (async () => {
   try {
     const result = await model.generateContent(prompt); 
     const response = result.response.text(); 
     console.log(response);  
-    const responseObject = { climateNews: response };
-    fs.writeFileSync('answer.json', JSON.stringify(responseObject, null, 4)); 
+    const responseJson = [{"climateNews" : response }];
+    fs.writeFileSync('answer.json', JSON.stringify(responseJson)); 
     console.log("Response saved to answer.json");
-
-
+    
   } catch (error) {
     console.error("Error generating content:", error);
   }
 })();
-
-
-
 
