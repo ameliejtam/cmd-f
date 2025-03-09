c92bffefe3977ee63e254c39ed804d4a39c4d004
eleanor <eleanort@students.cs.ubc.ca>
2025-03-08 19:52:37 -0800

    commitcheck

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
 console.log("email test");
 
-function getInputValue() {
+function getInputValueToJson() {
     // Get the input field value
     let inputValue = document.getElementById("userInput").value;
-    // Display the value
-    document.getElementById("output").innerText = "Entered Value: " + inputValue;
+    let jsonData = { "input": inputValue };
+
+    // Store JSON in LocalStorage
+    localStorage.setItem("userData", JSON.stringify(jsonData));
+
+     // Display confirmation
+    document.getElementById("output").innerText = "Data Saved: " + JSON.stringify(jsonData);
+
+
     // Log to console
     console.log(inputValue);
 }
